2717c7ace75d9cff5c3d88993b744c5529e90a08
Andrey Sokolov <andrey.sokolov@webriders.com.ua>
2011-09-21 12:02:20 +0400

    DWR-67 	Add files 'settings/local.py' and 'settings/deployment.py'

---
+++ site/source/settings/deployment.py
@@ -0,0 +1,8 @@
+"""
+Standalone Deployment settings to be used with --settings parameter
+"""
+
+# Load all apps settings
+from apps_settings import *
+from deployment_settings import *
+from main_settings import *
---
+++ site/source/settings/local.py
@@ -0,0 +1,8 @@
+"""
+Standalone Local settings to be used with --settings parameter
+"""
+
+# Load all apps settings
+from apps_settings import *
+from local_settings import *
+from main_settings import *
